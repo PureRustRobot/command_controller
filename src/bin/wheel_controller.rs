c50b498ab30908729a79adb7b83a3a323f23fa59
motii <108280115+motii8128@users.noreply.github.com>
2024-01-17 11:46:19 +0900

    Add files via upload

--- src/bin/wheel_controller.rs
+++ src/bin/wheel_controller.rs
@@ -1,11 +1,11 @@
 use async_std;
-use command_controller::wheel_controller;
+use command_controller::joy_to_cmd_vel;
 use zenoh::Error;
 
 #[async_std::main]
 async fn main()->Result<(), Error>
 {
-    let w_controller_task = async_std::task::spawn(wheel_controller("move_controller" ,"./param/wheel_controller.yaml"));
+    let w_controller_task = async_std::task::spawn(joy_to_cmd_vel("joy_to_wheel", "joy", "cmd/wheel"));
 
     w_controller_task.await?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,17 @@ use zenoh::{
     Error
 };
 
-use serde_json;
-use zenoh_interface::{CmdVel, dual_shock_4::Axis};
-use zenoh_manage_utils::param::get_str_param;
+use prr_msgs::msg::*;
 use zenoh_manage_utils::logger;
 
-pub async fn wheel_controller(node_name:&str, yaml_path:&str)->Result<(), Error>
+pub async fn joy_to_cmd_vel(
+    node_name:&str, 
+    sub_topic:&str,
+    pub_topic:&str,
+
+)->Result<(), Error>
 {
     let session = zenoh::open(Config::default()).res().await.unwrap();
-
-    let sub_topic = get_str_param(yaml_path, node_name, "sub_topic", "joy".to_string());
-    let pub_topic = get_str_param(yaml_path, node_name, "pub_topic", "wheel/cmd_vel".to_string());
 
     let subscriber = session.declare_subscriber(&sub_topic).res().await.unwrap();
     let publisher = session.declare_publisher(&pub_topic).res().await.unwrap();
@@ -26,16 +26,75 @@ pub async fn wheel_controller(node_name:&str, yaml_path:&str)->Result<(), Error>
     {
         let sample = subscriber.recv_async().await.unwrap();
                 
-        let axis:Axis = serde_json::from_str(&sample.value.to_string()).unwrap();
+        let get_data = deserialize_joystick(sample.value.to_string());
 
-        let send = CmdVel{
-            x:axis.joy_left_x,
-            y:axis.joy_left_y,
-            rotation_power:axis.joy_right_x
+        let cmd_vel = CmdVel{
+            x:get_data.left_x,
+            y:get_data.left_y,
+            rotation_power:get_data.right_x
         };
 
-        let buf = serde_json::to_string(&send).unwrap();
+        let serialized = serialize_cmdvel(&cmd_vel);
 
-        publisher.put(buf.clone()).res().await.unwrap();
+        logger::log_info(node_name, format!("Send:{}", serialized));
+
+        publisher.put(serialized).res().await.unwrap();
+    }
+}
+
+pub fn button_to_single_motor(
+    node_name:&str,
+    sub_topic:&str,
+    pub_topic:&str,
+    positive_name:&str,
+    negative_name:&str,
+)
+{
+    let session = zenoh::open(Config::default()).res().await.unwrap();
+
+    let subscriber = session.declare_subscriber(sub_topic).res().await.unwrap();
+    let publisher = session.declare_publisher(pub_topic).res().await.unwrap();
+
+    let msg = format!("Start sub:{}, pub:{}", subscriber.key_expr().to_string(), publisher.key_expr().to_string());
+    logger::log_info(node_name, msg);
+
+    loop {
+        let sample = subscriber.recv_async().await.unwrap();
+
+        let get_data = deserialize_buttons(sample.value.to_string());
+
+        let mut send_data = SingleMotor{
+            power:0.0
+        };
+
+        if name_to_button(positive_name, get_data) == 1.0
+        {
+            send_data.power = 1.0;
+        }
+        else if name_to_button(negative_name, get_data) == 1.0{
+            send_data.power = -1.0;
+        }
+    }
+}
+
+fn name_to_button(name:&str, btns:Buttons)->f32
+{
+    match name {
+        "circle"=>btns.circle,
+        "cross"=>btns.cross,
+        "cube"=>btns.cube,
+        "triangle"=>btns.triangle,
+        "up"=>btns.up_key,
+        "down"=>btns.down_key,
+        "left"=>btns.left_key,
+        "right"=>btns.right_key,
+        "r1"=>btns.r1,
+        "l1"=>btns.l1,
+        "r2"=>btns.r2,
+        "l2"=>btns.l2,
+        _=>{
+            logger::log_error("button_to_value", "Failed to get button value".to_string());
+            return 0.0
+        }
     }
 }
