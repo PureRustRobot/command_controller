f3899fb3ddefa2ecc3690ca1c03d8ad33d8ba34b
motii <108280115+motii8128@users.noreply.github.com>
2024-03-25 11:37:45 +0900

    Update wheel_controller.rs

--- src/bin/wheel_controller.rs
+++ src/bin/wheel_controller.rs
@@ -1,11 +1,11 @@
 use async_std;
-use command_controller::joy_to_cmd_vel;
+use command_controller::gamecon_to_cmd_vel;
 use zenoh::Error;
 
 #[async_std::main]
 async fn main()->Result<(), Error>
 {
-    let w_controller_task = async_std::task::spawn(joy_to_cmd_vel("joy_to_wheel", "joy", "cmd/wheel"));
+    let w_controller_task = async_std::task::spawn(gamecon_to_cmd_vel("joy_to_wheel", "joy", "cmd/wheel"));
 
     w_controller_task.await?;
 
